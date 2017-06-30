f5d1d96532c62e44f38cbcd1ad58df025bae5c3d
iii-user <iii-user@iii-userde-macbook-4.local>
2017-06-30 15:42:46 +0800

    c

--- MyApp15/ViewController.swift
+++ MyApp15/ViewController.swift
@@ -6,19 +6,49 @@
 //  Copyright © 2017年 iii-user. All rights reserved.
 //
 
+//1.info.plist => 權限
+//2.User => Permission
+
+
+
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    
+    
+    let lmgr = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let loc = locations.first {
+            let lat = loc.coordinate.latitude
+            let lng = loc.coordinate.longitude
+            let h = loc.altitude
+            print("\(lat),\(lng) = \(h)")
+            webView.stringByEvaluatingJavaScript(from: "moveToKD(\(lat),\(lng))")
+        }
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = Bundle.main.url(forResource: "map", withExtension: "html")
+        let req = URLRequest(url:url!)
+        webView.loadRequest(req)
+        
+        lmgr.requestAlwaysAuthorization()
+        lmgr.delegate = self
+        lmgr.startUpdatingLocation()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
